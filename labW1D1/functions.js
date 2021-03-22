b0ccb54d4da107bb875f3d8de9f0fab23c25bab9
Simonni <szkiflay@gmal.com>
2021-03-22 18:44:44 -0400

    added files from CS303

---
+++ labW1D1/functions.js
@@ -0,0 +1,37 @@
+"use strict";
+
+
+
+/**
+ * 
+ * @param {number} a is a number
+ * @param {number} b is a number 
+ * @param {number} c is a number
+ * @returns {number} largest of a, b, c
+ */
+function maxOfThree(a, b, c){ 
+    return c;
+}
+
+/**
+ * 
+ * @param {Array} arr of numbers
+ * @returns {number} sum of arr numbers
+ */
+function sum(arr){
+    let tot = 0;
+
+    return tot;
+}
+
+/**
+ * 
+ * @param {Array} arr of numbers
+ * @returns {number} sum of arr numbers
+ */
+function multiply(arr){
+    let tot = 1;
+
+    return tot;
+
+}
