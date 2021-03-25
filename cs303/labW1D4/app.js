a00127ecddc02d64b1af48bc16d02dd8afa47096
Simonni <szkiflay@gmal.com>
2021-03-25 18:05:08 -0400

    updated index.html

---
+++ cs303/labW1D4/app.js
@@ -0,0 +1,11 @@
+"use strict";
+
+/**
+ * 
+ * @param {string} str 
+ * @returns {string} returns the first Upper case of the string
+ */
+function ucFirst(str){
+return str.charAt(0).toUpperCase()
+}
+console.log(ucFirst('simon'))
