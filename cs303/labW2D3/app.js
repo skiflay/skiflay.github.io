18930af76a27b5311f0029aa997f58ff20451898
Simonni <szkiflay@gmal.com>
2021-04-02 16:36:01 -0400

    updated

--- cs303/labW2D3/app.js
+++ cs303/labW2D3/app.js
@@ -1,93 +1,112 @@
-"use strict";
-
-// exports.dblFun= dblFun;
-// exports.squareFun=squareFun;
-// exports.myMap = myMap;
-// exports.myReduce= myReduce;
-// exports.mulFun = mulFun;
-// exports.sumFun = sumFun;
-// exports.myFilter = myFilter;
-// exports.gratherThan1= gratherThan1;
-// exports.lessThan4= lessThan4;
-
-const arr = [1,2,3,4,5];
+"use strict"
+// module.exports = { sum,factorial,fibonacchi };
 /**
  * 
- * @param {Array} arr to be processed
- * @param {*} cbk to map the elements
- * @returns {Array} of mapped elements
+ * @param {number} num
+ * @returns{numner}number; 
  */
-function myMap(arr, cbk){
-//IMPLEMENTATION NEEDED
-let newArr = [];
-for(let n of arr){
-    newArr.push(cbk(n));
-}
-return newArr;
-}
-
-//const dblFun = num => num * 2;
-function dblFun(num){ return num*2}
-//const squareFun = num => num * num;
-function squareFun(num){return num *num}
-
-console.log("expect [1, 4, 6,8,10] ", arr.map(dblFun));
-console.log("expect [1, 4, 9, 16, 25] ", myMap(arr, squareFun));
-
-// console.log("expect [4, 16, 36, 64, 100] ", arr.map(squareFun));
-// console.log("expect [4, 16, 36, 64, 100] ", myMap(arr, squareFun));
-
+function sum(num){
+if(num===1){
+return num;
+ }
+else{
+return num+sum(num-1)
+ }
+ }
 /**
  * 
- * @param {Array} arr to be processed
- * @param {Function} cbk callback
- * @param {*} initialVal value for the reduction
- * @returns {*} the reduction of the array
- * go through the array, call cbk with accumulator and element
- * return value of cbk becomes accumulator for next loop
+ * @param {number} num
+ * @returns {number};
  */
-function myReduce(arr, cbk, initialVal){
-//IMPLEMENTATION NEEDED
-    let accum = initialVal;
-for(const ele of arr){
-    accum=cbk(accum, ele);
+function factorial(num){
+if(num===1){
+return num;
+ }
+else{
+return num * factorial(num-1);
+ }
 }
-return accum;
+//console.log(factorial(5));
+/**
+ * 
+ * @param {number} num
+ * @returns {number}number;
+ */
+function fibonacchi(num){
+if(num<=1){
+return num;
+ }
+else {
+return fibonacchi(num-1) + fibonacchi(num-2);
+ }
 }
-
-
-//const sumFun = (accum, val) => accum + val;
-function sumFun(accum=0, val){return accum + val};
-//const mulFun = (accum, val) => accum * val;
-function mulFun(accum=1, val){return accum * val};
-
-
-console.log("expect: 15: ", arr.reduce(sumFun, 0));
-console.log("expect: 15: ", myReduce(arr, sumFun, 0));
-
-console.log("expect: 120: ", arr.reduce(mulFun, 1));
-console.log("expect: 120: ", myReduce(arr, mulFun, 1));
 
 /**
  * 
- * @param {array} arr to be processed
- * @param {*} cbf to filter elements
- * @returns {Array} of filtered elements
+ * @returns {obj}
  */
-function myFilter(arr, cbf){
-    let newArr =[];
-    for(let ele of arr){
-        if(ele){
-            newArr.push(cbf(ele));
-        }
+function getListToPrint() {
+    return {
+    value: 1, next:
+    {
+    value: 2, next:
+    {
+    value: 3, next:
+    { value: 4, next: null }
     }
-    return newArr;
-}
+    }
+    };
+   }
+    
+   function printListbyRecursion(start) {
+    
+    console.log(start.value);
+    
+    let next = start.next;
+    
+    if (next !== null) printListbyRecursion(next);
+    
+   }
+    
+   function callprintListbyRecursion() { printListbyRecursion(getListToPrint()); }
+    
+   function printListbyIteration(start) {
+    
+    let retrn = [];
+    
+    while (start !== null) {
+    retrn.push(start.value);
+    start = start.next;
+    }
+    
+    retrn.forEach(n => log(n));
+   }
+    
+   function callprintListbyIteration() { printListbyIteration(getListToPrint()); }
 
-//const gratherThan1 = ele=> ele>1 
-function gratherThan1(ele){return ele>1 } 
-//const lessThan4 =ele=> ele<4;
-function lessThan4(ele){return ele<4}
-
-console.log("expecting [false, true,true,true, true]", myFilter(arr, gratherThan1))
-console.log("expecting [true, true,true,false, false]", myFilter(arr, lessThan4))
+   function reverseLinkedList(list) {
+ 
+    if (list.next !== null) reverseLinkedList(list.next);
+    console.log(list.value);
+    
+   }
+    
+   function callreverseLinkedList() { reverseLinkedList(getListToPrint()); }
+    
+   function reverseLinkedListIteration(list) {
+    
+    let li = [];
+    
+    while (true) {
+    li.push(list.value);
+    if (list.next === null) break;
+    list = list.next;
+    }
+    
+    for (let i = li.length - 1; i >= 0; i -= 1) log(li[i]);
+    
+   }
+    
+   function callreverseLinkedListIteration() { 
+    reverseLinkedListIteration(getListToPrint()); 
+   }
--- cs303/labW2D3/test.js
+++ cs303/labW2D3/test.js
@@ -1,33 +1,22 @@
-"use strict";
-// const assert = require("assert");
-// const pureFun = require("./app");
-
-describe("High order functions", function(){
-    const arr = [1,2,3,4,5];
-    it("multiply elements by 2", function(){
-        assert.deepEqual(myMap(arr, dblFun), [2,4,6,8,10])
-    });
-    it("double elements", function(){
-        assert.deepEqual(myMap(arr, squareFun), [1,4,9,16,25])
-});
-});
-
-describe("High order functions", function(){
-    const arr = [1,2,3,4,5];
-    it("sum elements of array", function(){
-        assert.deepEqual(myReduce(arr, sumFun), 15)
-    });
-    it("multiply elements of array", function(){
-        assert.deepEqual(myReduce(arr, mulFun), 120)
-    });
-});
-
-describe("High order functions", function(){
-    const arr = [1,2,3,4,5];
-    it("filter elements grather than 1", function(){
-        assert.deepEqual(myFilter(arr, gratherThan1), [false, true,true,true, true])
-    });
-    it("filter elements less than 4", function(){
-        assert.deepEqual(myFilter(arr, lessThan4), [true, true,true,false, false])
-    });
-});
+// "use strict"
+// const assert = require("assert"); //always need this with node
+// const myExports = require("./myRecursion.js");
+// const sum = myExports.sum;
+// const factorial = myExports.factorial;
+// const fibonacchi = myExports.fibonacchi;
+ 
+ 
+describe("numbers using recursion",function(){
+    it("get a sum number",function (){
+    assert.equal(sum(5),15)
+    })
+    
+    it("get a factor of a number",function (){
+    assert.equal(factorial(5),120)
+    })
+    
+    it("fibonacci of a number",function (){
+    assert.equal(fibonacchi(8),21)
+    })
+    
+    })
