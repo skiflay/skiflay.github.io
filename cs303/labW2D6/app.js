2d356f571bd3c7e2bc884a8cfd65e302d4613117
Simonni <szkiflay@gmal.com>
2021-04-04 19:53:01 -0400

    updated

---
+++ cs303/labW2D6/app.js
@@ -0,0 +1,63 @@
+"use strict";
+/* eslint-disable */
+
+/**
+ * 
+ * @param {number} from 
+ * @param {number} to 
+ * @returns {number} prints numbers from-to in 1 secord time interval
+ */
+function printNumbers(from, end){
+    let start = from;
+    let timerId = setInterval(()=>{
+        console.log(start);
+    if(start ===end){
+        clearInterval(timerId);
+    }
+    start++;
+}, 1000)    
+}
+//console.log("expect 1,2,3,4,5", printNumbers(1,5))
+
+
+/**
+ * 
+ * @param {number} from 
+ * @param {number} end
+ * @returns {number} prints numbers from-end in 1 secord time interval
+ */
+function printNumbers2(from, end){
+    let start = from;
+    setTimeout(function go(){
+        console.log(start)
+        if(start < end){
+            setTimeout(go, 1000)
+        }
+        start++;
+    },1000)
+}
+//console.log("expect 1,2,3,4,5", printNumbers2(1,5))
+
+
+//Write a makeBank function that will encapsulate and return the bank object.
+function makeBank(){
+    const bank = {};
+     bank.transactionsDB = [
+        { customerId: 1, customerTransactions: [10, 50, -40] },
+        { customerId: 2, customerTransactions: [10, 10, -10] },
+        { customerId: 3, customerTransactions: [5, -5, 55] }];
+     return function(){
+        return bank.getBalance = function(){
+            const totalBal = 0
+            for(let trans of bank.transactionsDB){
+                for(let bal of trans){
+                    totalBal += bal.customerTransactions;
+                }
+            }
+            return totalBal;
+        }
+     }
+}
+const checkBal= makeBank();
+console.log(checkBal())
+console.log(checkBal())
