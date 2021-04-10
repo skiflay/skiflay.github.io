e1b3c9820ba66b4d7d327b330d80d71c772115a0
Simonni <szkiflay@gmal.com>
2021-04-09 22:10:41 -0400

    added

---
+++ cs303/labW3D5/app.js
@@ -0,0 +1,26 @@
+"use strict"
+/* You need the module.exports when testing in node.  Comment it out when you send your file to the browser 
+*/
+module.exports = {Accumulator }; //add all of your function names here that you need for the node mocha tests
+
+
+/**
+ * 
+ * @param {number} initialValue is starting value
+ * @param {number} increment is amount added each call to accumulate
+ * @returns {Accumulator} constructor function
+ */
+function Accumulator(initialValue, increment){
+//IMPLEMENT THIS
+this.currentValue= initialValue;
+this.increment = increment;
+this.accumulate = function(){
+     this.currentValue += this.increment;
+     return this.currentValue;
+}
+this.report = function(){
+    return this.currentValue;
+}
+}
+const newAcc = new Accumulator(5,10)
+console.log(newAcc.accumulate())
